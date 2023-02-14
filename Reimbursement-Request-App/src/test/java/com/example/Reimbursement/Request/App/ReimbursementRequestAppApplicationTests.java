655c566b43085b572b48dd70af1b03591c6a5a9e
unknown <dylanmercer12@icloud.com>
2023-02-14 15:19:03 -0600

    project set up using spring boot and mongodb, docker file set up for mongodb and mongoexppress

---
+++ Reimbursement-Request-App/src/test/java/com/example/Reimbursement/Request/App/ReimbursementRequestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reimbursement.Request.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementRequestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
