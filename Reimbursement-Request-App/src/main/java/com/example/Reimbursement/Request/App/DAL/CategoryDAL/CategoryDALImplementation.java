8d9a40541e9ffb7b37f57885954beb4d25d7b024
unknown <dylanmercer12@icloud.com>
2023-03-02 01:15:53 -0600

    add category written but not yet tested

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/DAL/CategoryDAL/CategoryDALImplementation.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/DAL/CategoryDAL/CategoryDALImplementation.java
@@ -1,9 +1,11 @@
 package com.example.Reimbursement.Request.App.DAL.CategoryDAL;
 
 import com.example.Reimbursement.Request.App.Entities.Category;
+import com.example.Reimbursement.Request.App.Utilities.DatabaseConnection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.sql.*;
 import java.util.List;
 
 public class CategoryDALImplementation implements CategoryDALInterface {
@@ -11,7 +13,25 @@ public class CategoryDALImplementation implements CategoryDALInterface {
 
     @Override
     public Category addCategory(Category category) {
-        return null;
+        logger.info("Beginning DAL function add category with data: /n" + category);
+        try (Connection connection = DatabaseConnection.createConnection()) {
+            String sql = "insert into reimbursement_request_app.categories values (0, ?);";
+            PreparedStatement ps = null;
+            if (connection != null) {
+                ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, category.getCategoryName());
+                ps.execute();
+                ResultSet rs = ps.getGeneratedKeys();
+                rs.next();
+                category.setCategoryId(rs.getInt("category_id"));
+            }
+            logger.info("Finishing DAL function add category with result: /n" + category);
+            return category;
+        } catch (SQLException error) {
+            error.printStackTrace();
+            logger.error("Error with DAL function add category with error: " + error.getMessage());
+            return null;
+        }
     }
 
     @Override
