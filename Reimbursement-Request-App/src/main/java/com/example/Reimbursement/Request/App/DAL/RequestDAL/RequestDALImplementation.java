739db183814339e35af2cb382fc69bf4548e5897
unknown <dylanmercer12@icloud.com>
2023-03-12 01:46:47 -0600

    add request dal implemented but test file not rerun yet

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/DAL/RequestDAL/RequestDALImplementation.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/DAL/RequestDAL/RequestDALImplementation.java
@@ -7,9 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 @Service
@@ -35,7 +33,28 @@ public class RequestDALImplementation implements RequestDALInterface {
 
     @Override
     public Request addRequest(Request request) {
-        return null;
+        logger.info("Beginning DAL method add request with request: " + request);
+        try (Connection connection = DatabaseConnection.createConnection()) {
+            String sql = "insert into reimbursement_request_app.requests values (0, ?, ?, ?, ?);";
+            PreparedStatement ps = null;
+            if (connection != null) {
+                ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, request.getEmployeeId());
+                ps.setInt(2, request.getCategoryId());
+                ps.setString(3, request.getComment());
+                ps.setDouble(4, request.getAmount());
+                ps.execute();
+                ResultSet rs = ps.getGeneratedKeys();
+                rs.next();
+                request.setRequestId(rs.getInt(1));
+            }
+            logger.info("Finishing DAL method add request with result: " + request);
+            return request;
+        } catch (SQLException error) {
+            error.printStackTrace();
+            logger.error("Error with DAL method add request with error: " + error.getMessage());
+            return null;
+        }
     }
 
     @Override
