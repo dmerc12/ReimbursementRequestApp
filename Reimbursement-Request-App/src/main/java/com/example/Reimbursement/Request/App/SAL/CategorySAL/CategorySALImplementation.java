07b7b77459209b417233e765f85da4599e8d38b3
unknown <dylanmercer12@icloud.com>
2023-03-12 22:40:30 -0500

    category sal interface set up, imlementation also set up but nothing yet implemented

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/CategorySAL/CategorySALImplementation.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/CategorySAL/CategorySALImplementation.java
@@ -1,4 +1,32 @@
 package com.example.Reimbursement.Request.App.SAL.CategorySAL;
 
-public class CategorySALImplementation {
+import com.example.Reimbursement.Request.App.Entities.Category;
+
+import java.util.List;
+
+public class CategorySALImplementation implements CategorySALInterface{
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public Category getCategory(int categoryId) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public int deleteCategory(int categoryId) {
+        return 0;
+    }
 }
