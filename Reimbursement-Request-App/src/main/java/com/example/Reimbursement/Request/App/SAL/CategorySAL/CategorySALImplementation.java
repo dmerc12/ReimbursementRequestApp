ce8211a6ccf67ae9baa84cebe6b0fa4070910b04
unknown <dylanmercer12@icloud.com>
2023-03-13 00:11:30 -0500

    add category sal implemented, category sal tests not yet rerun

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/CategorySAL/CategorySALImplementation.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/CategorySAL/CategorySALImplementation.java
@@ -2,6 +2,7 @@ package com.example.Reimbursement.Request.App.SAL.CategorySAL;
 
 import com.example.Reimbursement.Request.App.DAL.CategoryDAL.CategoryDALImplementation;
 import com.example.Reimbursement.Request.App.Entities.Category;
+import com.example.Reimbursement.Request.App.Entities.CustomExceptions.GeneralError;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,11 +12,28 @@ public class CategorySALImplementation implements CategorySALInterface{
     private CategoryDALImplementation categoryDAO;
     public static Logger logger = LogManager.getLogger(CategorySALImplementation.class);
     public CategorySALImplementation(CategoryDALImplementation companyDAO) {
-        this.categoryDAO = categoryDAO;
+
     }
     @Override
     public Category addCategory(Category category) {
-        return null;
+        logger.info("Beginning SAL method add category with category: " + category);
+        if (category.getCategoryName().equals("")) {
+            logger.warn("SAL method add category, category name left empty");
+            throw new GeneralError("Category name field cannot be left empty, please try again!");
+        } else if (category.getCategoryName().length() > 60) {
+            logger.warn("SAL method add category, category name too long");
+            throw new GeneralError("Category name field cannot exceed 60 characters, please try again!");
+        } else {
+            List<Category> categoryList = getAllCategories();
+            if (categoryList.contains(category)) {
+                logger.warn("SAL method add category, category name already taken");
+                throw new GeneralError("Category with this name already exists, please try again!");
+            } else {
+                Category result = categoryDAO.addCategory(category);
+                logger.info("Finishing SAL method add category with result: " + result);
+                return result;
+            }
+        }
     }
 
     @Override
