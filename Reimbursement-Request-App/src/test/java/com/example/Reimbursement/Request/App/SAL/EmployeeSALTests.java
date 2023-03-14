a9f1807555f2cf93dccbee492276820bbf4dc4d0
unknown <dylanmercer12@icloud.com>
2023-03-13 20:12:43 -0500

    employee sal set up and ready to fill subsequent tests file with tests

--- Reimbursement-Request-App/src/test/java/com/example/Reimbursement/Request/App/SAL/EmployeeSALTests.java
+++ Reimbursement-Request-App/src/test/java/com/example/Reimbursement/Request/App/SAL/EmployeeSALTests.java
@@ -1,4 +1,10 @@
 package com.example.Reimbursement.Request.App.SAL;
 
+import com.example.Reimbursement.Request.App.DAL.EmployeeDAL.EmployeeDALImplementation;
+import com.example.Reimbursement.Request.App.SAL.EmployeeSAL.EmployeeSALImplementation;
+
 public class EmployeeSALTests {
+    int currentEmployeeId = 1;
+    EmployeeDALImplementation employeeDAO = new EmployeeDALImplementation();
+    EmployeeSALImplementation employeeSAO = new EmployeeSALImplementation(employeeDAO);
 }
