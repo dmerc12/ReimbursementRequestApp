173e1e253e90ff68711f52b1e0cbb8634f706199
unknown <dylanmercer12@icloud.com>
2023-03-22 17:09:49 -0500

    session sal interface complete, implementation set up, success test cases written and successfully fail for now

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/SessionSAL/SessionSALImplementation.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/SessionSAL/SessionSALImplementation.java
@@ -1,4 +1,37 @@
 package com.example.Reimbursement.Request.App.SAL.SessionSAL;
 
-public class SessionSALImplementation {
+import com.example.Reimbursement.Request.App.DAL.SessionDAL.SessionDALImplementation;
+import com.example.Reimbursement.Request.App.Entities.Session;
+import com.example.Reimbursement.Request.App.SAL.EmployeeSAL.EmployeeSALImplementation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SessionSALImplementation implements SessionSALInterface{
+    private SessionDALImplementation sessionDAO;
+    private EmployeeSALImplementation employeeSAO;
+    public static Logger logger = LogManager.getLogger(SessionSALImplementation.class);
+    public SessionSALImplementation(SessionDALImplementation sessionDAO, EmployeeSALImplementation employeeSAO) {
+        this.sessionDAO = sessionDAO;
+        this.employeeSAO = employeeSAO;
+    }
+
+    @Override
+    public Session addSession(Session session) {
+        return null;
+    }
+
+    @Override
+    public Session getSession(int sessionId) {
+        return null;
+    }
+
+    @Override
+    public Session updateSession(Session session) {
+        return null;
+    }
+
+    @Override
+    public int deleteSession(int sessionId) {
+        return 0;
+    }
 }
--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/SessionSAL/SessionSALInterface.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/SAL/SessionSAL/SessionSALInterface.java
@@ -1,4 +1,10 @@
 package com.example.Reimbursement.Request.App.SAL.SessionSAL;
 
+import com.example.Reimbursement.Request.App.Entities.Session;
+
 public interface SessionSALInterface {
+    Session addSession(Session session);
+    Session getSession(int sessionId);
+    Session updateSession(Session session);
+    int deleteSession(int sessionId);
 }
