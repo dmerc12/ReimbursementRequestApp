a7c05e7d3a88cb601b05cabe4655063d91abd039
unknown <dylanmercer12@icloud.com>
2023-04-02 23:44:18 -0500

    login controller created and tested with postman

--- Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/API/Main/LoginController.java
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/API/Main/LoginController.java
@@ -3,45 +3,47 @@ package com.example.Reimbursement.Request.App.API.Main;
 import com.example.Reimbursement.Request.App.DAL.EmployeeDAL.EmployeeDALImplementation;
 import com.example.Reimbursement.Request.App.DAL.SessionDAL.SessionDALImplementation;
 import com.example.Reimbursement.Request.App.Entities.CustomExceptions.GeneralError;
+import com.example.Reimbursement.Request.App.Entities.LoginRequest;
 import com.example.Reimbursement.Request.App.Entities.Session;
 import com.example.Reimbursement.Request.App.SAL.EmployeeSAL.EmployeeSALImplementation;
 import com.example.Reimbursement.Request.App.SAL.SessionSAL.SessionSALImplementation;
 import com.example.Reimbursement.Request.App.Entities.Employee;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
 public class LoginController {
+    public static Logger logger = LogManager.getLogger(LoginController.class);
 
     EmployeeDALImplementation employeeDAO = new EmployeeDALImplementation();
     EmployeeSALImplementation employeeSAO = new EmployeeSALImplementation(employeeDAO);
     SessionDALImplementation sessionDAO = new SessionDALImplementation();
     SessionSALImplementation sessionSAO = new SessionSALImplementation(sessionDAO, employeeSAO);
 
-    @GetMapping(value = {"/", "/login"})
-    public String renderLoginPage() {
-        return "Employee/login";
-    }
-
     @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password, Model model, HttpSession session) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
-            Employee employee = employeeSAO.login(email, password);
-            LocalDateTime newSessionExpiration = LocalDateTime.now().plusMinutes(15);
-            Session newSession = new Session(0, employee.getEmployeeId(), newSessionExpiration);
-            newSession = sessionSAO.addSession(newSession);
-            session.setAttribute("sessionId", newSession.getSessionId());
-            return "redirect:/Main/home";
+            logger.info("Beginning API method login with information: " + loginRequest);
+            Employee employee = employeeSAO.login(loginRequest.getEmail(), loginRequest.getPassword());
+            Session newSession = new Session(0, employee.getEmployeeId(), LocalDateTime.now().plusMinutes(15));
+            Session createdNewSession = sessionSAO.addSession(newSession);
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("sessionId", createdNewSession.getSessionId());
+            logger.info("Finishing API method login with result: " + responseBody);
+            return ResponseEntity.ok(responseBody);
         } catch (GeneralError error) {
-            model.addAttribute("errorMessage", error.getMessage());
-            return "Employee/login";
+            ResponseEntity<String> response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
+            logger.error("Error with API method login with error response: " + response);
+            return response;
         }
     }
 }
---
+++ Reimbursement-Request-App/src/main/java/com/example/Reimbursement/Request/App/Entities/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.Reimbursement.Request.App.Entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String email;
+    private String password;
+}
