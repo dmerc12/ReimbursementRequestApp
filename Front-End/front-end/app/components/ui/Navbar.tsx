c41c8f246ee1527322caa939eda5e75fa73ef665
unknown <dylanmercer12@icloud.com>
2023-04-06 01:53:13 -0500

    problem of separation between links on the nav has been fixed and now appears as I want, now the only issue with it other than the buttons not yet working because the pages do not yet exist is the fact that when the screen is small they stop being reactive and I want them to turn into a drop down menu that can be clicked to expand the options

--- Front-End/front-end/app/components/ui/Navbar.tsx
+++ Front-End/front-end/app/components/ui/Navbar.tsx
@@ -9,10 +9,12 @@ const Navbar = async ({}) => {
 
     return (
         <nav className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 bottom-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
-            <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
+            <div className='container max-w-7xl mx-auto w-full flex justify-left items-center'>
                 <Link href='/' className={buttonVariants({variant: 'link'})}>Home</Link>
                 <Link href='/manage-requests' className={buttonVariants({variant: 'link'})}>Manage Requests</Link>
                 <Link href='/manage-employee-information' className={buttonVariants({variant: 'link'})}>Manage Information</Link>
+            </div>
+            <div className='container max-w-7xl mx-auto w-full flex justify-end items-center'>
                 {/* <div className='md:hidden'>
                     <ThemeToggle></ThemeToggle>
                 </div>
@@ -27,8 +29,8 @@ const Navbar = async ({}) => {
                         <SignInButton>LogIn</SignInButton>
                         <Link href='/register' className={buttonVariants({variant: 'link'})}>Register</Link>
                     </>
-                }       
-            </div>
+                } 
+            </div>      
         </nav>
     )
 }
