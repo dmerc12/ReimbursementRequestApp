f3446a89a63a333ee96eb2428ac130e1045d50fb
unknown <dylanmercer12@icloud.com>
2023-04-27 02:26:11 -0500

    check now in place to stop a category from being deleted if it is being used in a reqeust avoiding a SQL foreign key error, delete category success postman test now fails due to the funcionality of deleting the requests not yet implemented

--- Back-End/src/main/java/Controllers/Category/DeleteCategoryController.java
+++ Back-End/src/main/java/Controllers/Category/DeleteCategoryController.java
@@ -2,11 +2,14 @@ package Controllers.Category;
 
 import DAL.CategoryDAL.CategoryDALImplementation;
 import DAL.EmployeeDAL.EmployeeDALImplementation;
+import DAL.RequestDAL.RequestDALImplementation;
 import DAL.SessionDAL.SessionDALImplementation;
 import Entities.CustomExceptions.GeneralError;
+import Entities.Data.Request;
 import Entities.Data.Session;
 import SAL.CategorySAL.CategorySALImplementation;
 import SAL.EmployeeSAL.EmployeeSALImplementation;
+import SAL.RequestSAL.RequestSALImplementation;
 import SAL.SessionSAL.SessionSALImplementation;
 import com.google.gson.Gson;
 import io.javalin.http.Handler;
@@ -16,6 +19,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 
 public class DeleteCategoryController {
     public static Logger logger = LogManager.getLogger(GetCategoryController.class);
@@ -23,6 +27,8 @@ public class DeleteCategoryController {
     EmployeeDALImplementation employeeDAO = new EmployeeDALImplementation();
     EmployeeSALImplementation employeeSAO = new EmployeeSALImplementation(employeeDAO);
     CategorySALImplementation categorySAO = new CategorySALImplementation(categoryDAO, employeeSAO);
+    RequestDALImplementation requestDAO = new RequestDALImplementation();
+    RequestSALImplementation requestSAO = new RequestSALImplementation(requestDAO, employeeSAO, categorySAO);
     SessionDALImplementation sessionDAO = new SessionDALImplementation();
     SessionSALImplementation sessionSAO = new SessionSALImplementation(sessionDAO, employeeSAO);
     public Handler deleteCategory = ctx -> {
@@ -32,6 +38,13 @@ public class DeleteCategoryController {
             logger.info("Beginning API handler delete category with info: " + categoryId + ", " + sessionId);
             Gson gson = new Gson();
             Session currentSession = sessionSAO.getSession(sessionId);
+            List<Request> allEmployeeRequests = requestSAO.getAllRequests(currentSession.getEmployeeId());
+            for (Request request : allEmployeeRequests) {
+                if (request.getCategoryId() == categoryId) {
+                    throw new GeneralError("This category is being used in a request elsewhere and cannot be " +
+                            "deleted, please try again!");
+                }
+            }
             int result = categorySAO.deleteCategory(categoryId);
             Session updatedSessionInfo = new Session(currentSession.getSessionId(), currentSession.getEmployeeId(),
                     LocalDateTime.now().plusMinutes(15));
