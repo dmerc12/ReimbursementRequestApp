ccd4992bb4df004643c045104dee50ffdadb752e
unknown <dylanmercer12@icloud.com>
2023-05-06 16:17:19 -0500

    more progress made with category list component with a table head now created though the text is grey and dim and doesn't pop like I would like, also in the last I added a metadata export to the changing password page so that the title and description change and there is an example to be used with the other pages

--- Front-End/src/components/ui/category/CategoryList.jsx
+++ Front-End/src/components/ui/category/CategoryList.jsx
@@ -1,8 +1,21 @@
 export default function CategoryList() {
     return (
         <>
-            <div className="category-list">
-                
+            <div className="category-list overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th  className="category-table-head">Category Name</th>
+                            <th  className="category-table-head">Update Category</th>
+                            <th  className="category-table-head">Delete Category</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
