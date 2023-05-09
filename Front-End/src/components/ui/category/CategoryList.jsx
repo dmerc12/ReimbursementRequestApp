99655939fcf3604eaefb226bbbc4ecd468be1a89
unknown <dylanmercer12@icloud.com>
2023-05-09 10:53:47 -0500

    worked a bit on the category list, trying to make the component async to get the categories and display them in the list, to do so the sessionId needs to be grabbed on the page and sent to the component so that it can make use of that and get the categories, moving computer to storage today so a  break may be taken....

--- Front-End/src/components/ui/category/CategoryList.jsx
+++ Front-End/src/components/ui/category/CategoryList.jsx
@@ -1,4 +1,19 @@
-export default function CategoryList() {
+'use client'
+
+export default async function CategoryList({ sessionId }) {
+    const getAllCategories = async => {
+        try {
+            const categories = await fetch("/api/category/handleGetAll", {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'}, 
+                body: JSON.stringify({
+                    'sessionId': sessionId
+                })
+        } catch (error) {
+
+        }
+        
+    })}
     return (
         <>
             <div className="category-list overflow-x-auto">
@@ -6,13 +21,11 @@ export default function CategoryList() {
                     <thead>
                         <tr>
                             <th  className="category-table-head">Category Name</th>
-                            <th  className="category-table-head">Update Category</th>
-                            <th  className="category-table-head">Delete Category</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            
+                            {categories.map()}
                         </tr>
                     </tbody>
                 </table>
