18efa4cb47064d8175ca594ea55451c81bb4e45d
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-05-20 02:50:31 -0500

    add category form added to model and put with the button as a separate component, manually tested and works, successfully closes modal and shows a toast notification on success, untested for error messages

--- Front-End/front-end/components/Model.jsx
+++ Front-End/front-end/components/Model.jsx
@@ -11,10 +11,9 @@ const Modal = ({ visible, onClose, children }) => {
             <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" id="wrapper" onClick={handleClose}>
                 <div className="w-[600px] flex flex-col">
                 <button className="text-white text-xl place self-end" onClick={() => onClose()}>x</button>
-                    <div className="bg-white p-2 rounded">
+                    <div className="bg-black p-2 rounded">
                         {children}
                     </div>
-
                 </div>
             </div>
         </>
--- Front-End/front-end/pages/manage-categories/index.jsx
+++ Front-End/front-end/pages/manage-categories/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify';
-import AddCategoryButton from '@/components/ui/category/AddCategoryButton';
+import AddCategoryComponent from '@/components/ui/category/AddCategoryComponent';
 import CategoryList from '@/components/ui/category/CategoryList';
 
 export default function ManageInformation() {
@@ -10,10 +10,11 @@ export default function ManageInformation() {
 
   const [categories, setCategories] = useState([]);
 
+  const sessionId = document.cookie.split(';').find(cookie => cookie.trim().startsWith('sessionId=')).split('=')[1];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const sessionId = document.cookie.split(';').find(cookie => cookie.trim().startsWith('sessionId=')).split('=')[1];
         if (!sessionId) {
           router.push('/login');
           toast.info("Please login or register to gain access!", {
@@ -27,8 +28,7 @@ export default function ManageInformation() {
               'sessionId': sessionId
             })})
           const data = await response.json();
-          console.log(data)
-          setCategories(data)
+          setCategories(data);
         }
       } catch (error) {
         toast.error(error.message, {
@@ -42,7 +42,7 @@ export default function ManageInformation() {
   return (
     <>
       <h1>Manage Category Information Page</h1>
-      <AddCategoryButton />
+      <AddCategoryComponent sessionId={sessionId}/>
       <CategoryList categories={categories}/>
     </>
   )
