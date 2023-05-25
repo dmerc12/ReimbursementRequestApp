7b04de0b77882cd3ae563ab2fe4158ab2a7a737f
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-05-25 00:10:26 -0500

    tested new routes with postman and they work as intended, created handler files to be used in the front end components

--- Front-End/front-end/pages/api/request/handleGetAll.js
+++ Front-End/front-end/pages/api/request/handleGetAll.js
@@ -1,15 +1,15 @@
 const handler = async (req, res) => {
     const sessionId = req.body.sessionId;
-    
+
     const method = 'GET';
     
     const requestOptions = {
         method: method,
-        headers: {'Content-Type': 'application/json'},
-    };
-    
-    const URL = `http://localhost:8080/get/all/categories/${sessionId}`;
-    
+        headers: {'Content-Type': 'application/json'}
+    }
+
+    const URL = `http://localhost:8080/get/all/requests/${sessionId}`;
+
     try {
         const response = await fetch(URL, requestOptions);
         const data = await response.json();
@@ -18,11 +18,11 @@ const handler = async (req, res) => {
         } else if (response.status === 400) {
             return res.end(JSON.stringify({'error': data}));
         } else {
-            return res.end(JSON.stringify({'error': "Cannot connect to the back end, please try again!"}));
-         }
+            return res.end(JSON.stringify({'error': 'Cannot connect to the back end, please try again!'}));
+        }
     } catch (error) {
         return res.end(JSON.stringify({'error': error.message}));
-     }
+    }
 }
-  
+
 export default handler;
