5f9a7058cc1d0068706fd965cdee927ce9c24fe1
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-05-26 13:48:07 -0500

    request list not complete but built to where it can currently, all other components for manage requests page created but not complete

---
+++ Front-End/front-end/components/ui/request/RequestList.jsx
@@ -0,0 +1,40 @@
+export default function RequestList({ sessionId, requests }) {
+    let requestRows = [];
+    if (requests.success) {
+        for (let i=0; i<requests.success.length; i++) {
+            const request = requests.success[i];
+            requestRows.unshift(
+                <tr key={request.requestId}>
+                    <td className='request-table-data w-full'>{request.requestId}</td>
+                    <td className='request-table-data w-full'>{request.categoryId}</td>
+                    <td className='request-table-data w-full'>{request.comment}</td>
+                    <td className='request-table-data w-full'>{request.amount}</td>
+                    <td className='request-table-data flex gap-5'>
+                        
+                    </td>
+                </tr>
+            )
+        }
+    }
+
+    return (
+        <>
+            <div className="request-list overflow-x-auto">
+                <table className='table w-full'>
+                    <thead>
+                        <tr>
+                            <th className="request-table-head">Request ID</th>
+                            <th className="request-table-head">Category</th>
+                            <th className="request-table-head">Comment</th>
+                            <th className="request-table-head">Amount</th>
+                            <th className="request-table-head">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {requestRows}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
