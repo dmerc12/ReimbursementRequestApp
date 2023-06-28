90f2113cae679fce1ebac4235be281a1eb928900
unknown <dylanmercer12@icloud.com>
2023-06-28 15:42:20 -0500

    created a small python project consisting of a python file, and 2 text files in which it takes one unformatted text file containing the information from a web page containing the zip codes I am getting and extracting soley the zip codes and outputting them as a list of string zip codes, used it to fill california's zip codes

---
+++ Front-End/ZipCodeExtracter/ZipCodeExtracter/extract.py
@@ -0,0 +1,19 @@
+with open('unformatted.txt', 'r') as file:
+    original_data = file.readlines()
+
+def extract_zip_codes():
+    zip_codes = []
+
+    for line in original_data:
+        values = line.split('\t')
+        zip_code = values[0].strip()
+
+        if zip_code.isdigit():
+            zip_codes.append(zip_code)
+
+    return "', '".join(zip_codes)
+
+extracted_zip_codes = extract_zip_codes()
+
+with open('formatted.txt', 'w') as file:
+    file.write("'" + extracted_zip_codes + "'")
