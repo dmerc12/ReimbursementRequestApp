ed3ea86c07628c472161184fe23ef12ba55fce17
unknown <dylanmercer12@icloud.com>
2023-07-03 22:38:11 -0500

    update employee info form updated to use new address structure adapted in the register form, also set them to automatically set to the current address still after breaking it down, similar to how it's sent across now but in reverse

--- Front-End/front-end/components/ui/employee/UpdateForm.jsx
+++ Front-End/front-end/components/ui/employee/UpdateForm.jsx
@@ -7,14 +7,55 @@ import Modal from '@/components/Model';
 import Cookies from 'js-cookie';
 
 export default function UpdateEmployeeForm({ employee }) {
+    const addressComponents = employee.address.split(', ');
+    const initialStreetAddress = addressComponents[0];
+    const initialCity = addressComponents[1];
+    const initialState = addressComponents[2].split(' ')[0];
+    const initialZipCode = addressComponents[2].split(' ')[1];
+
     const [firstName, setFirstName] = useState(employee.firstName);
     const [lastName, setLastName] = useState(employee.lastName);
     const [email, setEmail] = useState(employee.email);
     const [phoneNumber, setPhoneNumber] = useState(employee.phoneNumber);
-    const [address, setAddress] = useState(employee.address);
+    const [streetAddress, setStreetAddress] = useState(initialStreetAddress);
+    const [city, setCity] = useState(initialCity);
+    const [state, setState] = useState(initialState);
+    const [zipCode, setZipCode] = useState(initialZipCode);
+    const [states, setStates] = useState([]);
+    const [zipCodes, setZipCodes] = useState([]);
     const [visible, setVisible] = useState(false);
 
     const router = useRouter();
+
+    const fetchStates = async () => {
+        try {
+            const response = await fetch('/api/states');
+            const data = await response.json();
+            setStates(data.states);
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    };
+
+    const fetchZipCodes = async (state) => {
+        try {
+            const response = await fetch(`/api/zipcodes?state=${state}`);
+            const data = await response.json();
+            setZipCodes(data.zipCodes);
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    };
+
+    const handleStateChange = (event) => {
+        setState(event.target.value);
+        fetchZipCodes(event.target.value);
+    };
+
+    const handleZipCodeChange = (event) => {
+        setZipCode(event.target.value);
+
+    }
 
     const handlePhoneNumberChange = (event) => {
         const phoneNumberValue = event.target.value;
@@ -35,11 +76,14 @@ export default function UpdateEmployeeForm({ employee }) {
     useEffect(() => {
         const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
         setPhoneNumber(formattedPhoneNumber);
+        fetchStates();
     }, [phoneNumber]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
+            const fullAddress = `${streetAddress}, ${city}, ${state} ${zipCode}`;
+
             const sessionIdCookie = Cookies.get('sessionId');
             const response = await fetch('/api/employee/handleUpdate', 
                 {
@@ -51,10 +95,10 @@ export default function UpdateEmployeeForm({ employee }) {
                         'lastName': lastName,
                         'email': email,
                         'phoneNumber': phoneNumber,
-                        'address': address
+                        'address': fullAddress
                     })
                 }
-            )
+            );
             const data = await response.json();
 
             if (data.success) {
@@ -68,7 +112,7 @@ export default function UpdateEmployeeForm({ employee }) {
             } else if (data.error) {
                 throw new Error(`${data.error}`);
             } else {
-                throw new Error("Something went extremely wrong, please try again!")
+                throw new Error("Something went extremely wrong, please try again!");
             }
         } catch (error) {
             if (error.message === "Session has expired, please log in!") {
@@ -115,8 +159,45 @@ export default function UpdateEmployeeForm({ employee }) {
                     </div>
 
                     <div className='form-field'>
-                        <label className='form-label' htmlFor="address">Address: </label>
-                        <input className='form-input' type="text" id='updateAddress' name='address' value={address} onChange={event => setAddress(event.target.value)}/>
+                        <label className='form-label' htmlFor="streetAddress">Street Address: </label>
+                        <input className='form-input' type="text" id='updateStreetAddress' name='streetAddress' value={streetAddress} onChange={event => setStreetAddress(event.target.value)}/>
+                    </div>
+
+                    <div className='form-field'>
+                        <label className='form-label' htmlFor='city'>City: </label>
+                        <input className='form-input' type='text' id='updateCity' name='city' value={city} onChange={(event) => setCity(event.target.value)} />
+                    </div>
+
+                    <div className='form-field'>
+                        <label className='form-label' htmlFor='state'>State: </label>
+                        <select className='form-input' id='updateState' name='state' value={state} onChange={handleStateChange}>
+                            {states.length > 0 && (
+                                (() => {
+                                    const options = [];
+                                    for (let i = 0; i < states.length; i++) {
+                                        const state = states[i];
+                                        options.push(<option key={state.code} value={state.code}>{state.name}</option>);
+                                    }
+                                    return options;
+                                })
+                            )}
+                        </select>
+                    </div>
+
+                    <div className='form-field'>
+                        <label className='form-label' htmlFor='zipCode'>Zip Code: </label>
+                        <select className='form-input' id='updateZipCode' name='zipCode' value={zipCode} onChange={handleZipCodeChange}>
+                            {zipCodes.length > 0 && (
+                                (() => {
+                                    const options = [];
+                                    for (let i = 0; i < zipCodes.length; i++) {
+                                        const zipCode = zipCodes[i];
+                                        options.push(<option key={i} value={zipCode}>{zipCode}</option>);
+                                    }
+                                    return options;
+                                })
+                            )}
+                        </select>
                     </div>
 
                     <button className='form-btn-2' type='submit' id='updateCurrentInformationButton'>Update Current Information</button>
