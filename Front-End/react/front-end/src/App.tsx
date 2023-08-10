0fd033fbc1697b36f2863ac56317a8d251993ee2
unknown <dylanmercer12@icloud.com>
2023-08-09 22:44:28 -0500

    discovered I was missing the react toastify css import where I have my toast container

--- Front-End/react/front-end/src/App.tsx
+++ Front-End/react/front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
