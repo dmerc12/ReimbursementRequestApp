baa6cacbe6d0206f4820f07d7c03d1e60ea626f1
unknown <dylanmercer12@icloud.com>
2023-08-12 00:41:21 -0500

    home page complete including limiting access to only those who are logged in

--- Front-End/react/front-end/src/pages/Home.tsx
+++ Front-End/react/front-end/src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { Modal } from '../components/Modal';
-import { useState } from 'react'
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+    const [sessionId, setSessionId] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const sessionIdCookie = Cookies.get('sessionId');
+        if (!sessionIdCookie) {
+            navigate('/login')
+            toast.info("Please login or register to gain access!", {
+                toastId: 'customId'
+            })
+        } else {
+            setSessionId(sessionIdCookie);
+        }
+    }, [])
+
     return (
         <>
-            <h1>home</h1>
-
-            
+            <div className="nav-btn-container">
+                <Link className="nav-btn" to='/manage-requests'>Manage Request Information</Link>
+                <Link className="nav-btn" to='/manage-categories'>Manage Categories</Link>
+                <Link className="nav-btn" to='/manage-information'>Manage Information</Link>
+            </div>
         </>
     );
 }
