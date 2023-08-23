5f43f2e9dc4d5853e9018ebcb88f6e6e5122f9e5
unknown <dylanmercer12@icloud.com>
2023-08-22 20:56:44 -0500

    functions added to request list component, only missing the jsx portion and finishing the fetch requests function try block

--- Front-End/react/front-end/src/components/ui/request/RequestList.tsx
+++ Front-End/react/front-end/src/components/ui/request/RequestList.tsx
@@ -22,7 +22,82 @@ export const RequestList = () => {
 
     const navigate = useNavigate();
 
-    let requestRows = []
+    let requestRows = [];
+
+    const fetchCategories = async () => {
+        try {
+            setIsLoading(true);
+            setFailedToFetch(false);
+
+            const sessionId = Cookies.get('sessionId');
+                
+            const response = await fetch(`http://localhost:8080/get/all/categories/${sessionId}`, {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'}
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+                setCategories(data);
+                setIsLoading(false);
+            } else if (response.status === 400) {
+                throw new Error(`${data.message}`);
+            } else {
+                throw new Error('Cannot connect to the back end, please try again!');
+            }
+        } catch (error: any) {
+            if (error.message === "No session found, please try again!" || error.message === "Session has expired, please log in!") {
+                Cookies.remove('sessionId');
+                navigate('/login');
+                setIsLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            } else if (error.message === "Failed to fetch") {
+                setFailedToFetch(true);
+                setIsLoading(false);
+            } else {
+                setIsLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            }
+        }
+    }
+
+    const fetchRequests = async () => {
+        try {
+
+        } catch (error: any) {
+            if (error.message === "No session found, please try again!" || error.message === "Session has expired, please log in!") {
+                Cookies.remove('sessionId');
+                navigate('/login');
+                setIsLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            } else if (error.message === "Failed to fetch") {
+                setFailedToFetch(true);
+                setIsLoading(false);
+            } else {
+                setIsLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            }
+        }
+    }
+
+    const goBack = () => {
+        navigate('/home');
+        setFailedToFetch(false);
+    }
+
+    useEffect(() => {
+        fetchCategories();
+        fetchRequests();
+    }, [])
 
     return (
         <>
