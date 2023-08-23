182c69d39299c267196ea6989db3ac28e2d615f7
unknown <dylanmercer12@icloud.com>
2023-08-22 20:50:47 -0500

    request type created, began declaring variables in the request list component, secured the manage requests page similar to the manage categories and manage information pages

--- Front-End/react/front-end/src/components/ui/request/RequestList.tsx
+++ Front-End/react/front-end/src/components/ui/request/RequestList.tsx
@@ -1,4 +1,29 @@
+import { UpdateRequest } from "./UpdateRequest";
+import { DeleteRequest } from "./DeleteRequest";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { toast } from 'react-toastify';
+import { FaSpinner, FaSync } from 'react-icons/fa';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import Cookies from "js-cookie";
+
+export interface Request {
+    requestId: number,
+    categoryId: number,
+    comment: string,
+    amount: number
+}
+
 export const RequestList = () => {
+    const [categories, setCategories] = useState([]);
+    const [requests, setRequests] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [failedToFetch, setFailedToFetch] = useState(false);
+
+    const navigate = useNavigate();
+
+    let requestRows = []
+
     return (
         <>
         
