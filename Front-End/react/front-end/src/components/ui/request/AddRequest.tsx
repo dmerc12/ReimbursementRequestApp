64393e0b5da704c2a142857d5eca22dfa92767e4
unknown <dylanmercer12@icloud.com>
2023-08-25 22:50:26 -0500

    added functionality to handle failedToFetch for both the categories and submission of the form, I'll need to catch the rest of the project up with that where needed

--- Front-End/react/front-end/src/components/ui/request/AddRequest.tsx
+++ Front-End/react/front-end/src/components/ui/request/AddRequest.tsx
@@ -1,6 +1,6 @@
 import { AiOutlinePlus, AiOutlineExclamationCircle } from "react-icons/ai";
 import { FaSpinner, FaSync } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Modal } from "../../Modal";
@@ -10,36 +10,92 @@ import { Category } from "../category/CategoryList";
 export const AddRequest = () => {
     const [visible, setVisible] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [failedToFetch, setFailedToFetch] = useState(false);
+    const [failedToFetchCategories, setFailedToFetchCategories] = useState(false);
+    const [failedToFetchSubmission, setFailedToFetchSubmission] = useState(false)
     const [categoryId, setCategoryId] = useState(0);
     const [comment, setComment] = useState('');
     const [amount, setAmount] = useState(0.00);
     const [categories, setCategories] = useState<Category[]>([])
 
-    const fetchCategories = async () => {
+    const navigate = useNavigate();
 
+    const fetchCategories = async () => {
+        try {
+            setLoading(true);
+            setFailedToFetchCategories(false);
+
+            const sessionId = Cookies.get('sessionId');
+                
+            const response = await fetch(`http://localhost:8080/get/all/categories/${sessionId}`, {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'}
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+                setCategories(data);
+                setLoading(false);
+            } else if (response.status === 400) {
+                throw new Error(`${data.message}`);
+            } else {
+                throw new Error('Cannot connect to the back end, please try again!');
+            }
+        } catch (error: any) {
+            if (error.message === "No session found, please try again!" || error.message === "Session has expired, please log in!") {
+                Cookies.remove('sessionId');
+                navigate('/login');
+                setLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            } else if (error.message === "Failed to fetch") {
+                setFailedToFetchCategories(true);
+                setLoading(false);
+            } else {
+                setLoading(false);
+                toast.warn(error.message, {
+                    toastId: "customId"
+                });
+            }
+        }
     }
 
     const onSubmit = async (event: any) => {
-
+        
     }
 
     const goBack = () => {
-
+        setFailedToFetchCategories(false);
+        setFailedToFetchSubmission(false);
     }
+
+    useEffect(() => {
+        fetchCategories();
+    }, [])
 
     return ( 
         <>
             <div className="component">
-                <button onClick={() => {setVisible(true); setFailedToFetch(false)}} className="action-btn" id="addRequestModal">Add New Request <AiOutlinePlus className="plus-icon" size={15}/></button>
+                <button onClick={() => {setVisible(true); setFailedToFetchCategories(false); setFailedToFetchSubmission(false)}} className="action-btn" id="addRequestModal">Add New Request <AiOutlinePlus className="plus-icon" size={15}/></button>
             </div>
 
-            <Modal visible={visible} onClose={() => {setVisible(false); setFailedToFetch(false)}}>
+            <Modal visible={visible} onClose={() => {setVisible(false); setFailedToFetchCategories(false); setFailedToFetchSubmission(false)}}>
                 {loading ? (
                     <div className="loading-indicator">
                         <FaSpinner className="spinner"/>
                     </div>
-                ) : failedToFetch ? (
+                ) : failedToFetchCategories ? (
+                    <div className='failed-to-fetch'>
+                        <AiOutlineExclamationCircle className='warning-icon'/>
+                        <p>Cannot connect to the back end server.</p>
+                        <p>Please check your internet connection and try again.</p>
+                        <button className='retry-button' onClick={fetchCategories}>
+                            <FaSync className='retry-icon'/> Retry
+                        </button>
+                        <button className='back-button' onClick={goBack}>Go Back</button>
+                    </div>  
+                ) : failedToFetchSubmission ? (
                     <div className='failed-to-fetch'>
                         <AiOutlineExclamationCircle className='warning-icon'/>
                         <p>Cannot connect to the back end server.</p>
@@ -48,7 +104,7 @@ export const AddRequest = () => {
                             <FaSync className='retry-icon'/> Retry
                         </button>
                         <button className='back-button' onClick={goBack}>Go Back</button>
-                    </div>  
+                    </div>
                 ) : (
                     <form className="form" onSubmit={onSubmit}>
                         <div className="form-field">
