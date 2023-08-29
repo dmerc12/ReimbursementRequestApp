a235521f73f19e1b1a74d01aed2cd0e48cf190ea
unknown <dylanmercer12@icloud.com>
2023-08-28 19:30:47 -0500

    login form refactored to use loading rather than isLoading also altered button to be a normal button since entire form will turn into loading icon on submit, added id tags to all elements in navbar so they can be selected by selenium, created poms for login page and navbar

--- Back-End/src/test/java/POMs/EmployeePOMs.java
+++ Back-End/src/test/java/POMs/EmployeePOMs.java
@@ -5,13 +5,48 @@ import org.openqa.selenium.WebDriver;
 
 public class EmployeePOMs {
     private WebDriver driver;
-    private By loginEmailInput = By.id("element-id(not currently the real id)");
+    private By registerTab = By.id("registerTab");
+    private By loginTab = By.id("loginTab");
+    private By manageRequestsTab = By.id("manageRequestsTab");
+    private By manageCategoriesTab = By.id("manageCategoriesTab");
+    private By manageInformationTab = By.id("manageInformationTab");
+    private By loginEmailInput = By.id("loginEmail");
+    private By loginPasswordInput = By.id("loginPassword");
+    private By loginButton = By.id("loginButton");
 
     public EmployeePOMs(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void loginEmailInput(String comment) {
-        driver.findElement(loginEmailInput).sendKeys(comment);
+    public void clickRegisterTab() {
+        driver.findElement(registerTab).click();
+    }
+
+    public void clickLoginTab() {
+        driver.findElement(loginTab).click();
+    }
+
+    public void clickManageRequestsTab() {
+        driver.findElement(manageRequestsTab).click();
+    }
+
+    public void clickManageCategoriesTab() {
+        driver.findElement(manageCategoriesTab).click();
+    }
+
+    public void clickManageInformationTab() {
+        driver.findElement(manageInformationTab).click();
+    }
+
+    public void setLoginEmailInput(String email) {
+        driver.findElement(loginEmailInput).sendKeys(email);
+    }
+
+    public void setLoginPasswordInput(String password) {
+        driver.findElement(loginPasswordInput).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
     }
 }
