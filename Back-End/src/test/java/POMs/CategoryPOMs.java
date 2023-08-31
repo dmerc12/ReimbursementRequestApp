c2f3cdf220f637715390c8367723a0ee28f504f4
unknown <dylanmercer12@icloud.com>
2023-08-31 09:14:09 -0500

    category poms now include elements for add category component

--- Back-End/src/test/java/POMs/CategoryPOMs.java
+++ Back-End/src/test/java/POMs/CategoryPOMs.java
@@ -7,6 +7,9 @@ public class CategoryPOMs {
     private WebDriver driver;
     private By manageCategoriesTab = By.id("manageCategoriesTab");
     private By manageCategoriesButton = By.id("manageCategoriesButton");
+    private By addCategoryModal = By.id("addCategoryModal");
+    private By newCategoryName = By.id("newCategoryName");
+    private By addCategoryButton = By.id("addCategoryButton");
 
     public CategoryPOMs(WebDriver driver) {
         this.driver = driver;
@@ -19,6 +22,17 @@ public class CategoryPOMs {
 
     public void clickManageCategoriesButton() {
         driver.findElement(manageCategoriesButton).click();
+    }
+    public void clickAddCategoryModal() {
+        driver.findElement(addCategoryModal).click();
+    }
+
+    public void setNewCategoryName(String categoryName) {
+        driver.findElement(newCategoryName).sendKeys(categoryName);
+    }
+
+    public void clickAddCategoryButton() {
+        driver.findElement(addCategoryButton).click();
     }
 
 }
