0d26a0a9670b613240c578f59396e2a8408179bf
unknown <dylanmercer12@icloud.com>
2023-08-31 09:33:33 -0500

    request poms now includes elements for add request

--- Back-End/src/test/java/POMs/RequestPOMs.java
+++ Back-End/src/test/java/POMs/RequestPOMs.java
@@ -8,6 +8,11 @@ public class RequestPOMs {
     private By manageRequestsTab = By.id("manageRequestsTab");
 
     private By manageRequestsButton = By.id("manageRequestsButton");
+    private By addRequestModal = By.id("addRequestModal");
+    private By addRequestCategoryDropdown = By.id("addRequestCategoryDropdown");
+    private By addRequestComment = By.id("addRequestComment");
+    private By addRequestAmount = By.id("addRequestAmount");
+    private By addRequestButton = By.id("addRequestButton");
 
     public RequestPOMs(WebDriver driver) {
         this.driver = driver;
@@ -19,5 +24,24 @@ public class RequestPOMs {
 
     public void clickManageRequestsButton() {
         driver.findElement(manageRequestsButton).click();
+    }
+    public void clickAddRequestModal() {
+        driver.findElement(addRequestModal).click();
+    }
+
+    public void setAddRequestCategoryDropdown(String category) {
+        driver.findElement(addRequestCategoryDropdown).sendKeys(category);
+    }
+
+    public void setAddRequestComment(String comment) {
+        driver.findElement(addRequestComment).sendKeys(comment);
+    }
+
+    public void setAddRequestAmount(double amount) {
+        driver.findElement(addRequestAmount).sendKeys(String.valueOf(amount));
+    }
+
+    public void clickAddRequestButton() {
+        driver.findElement(addRequestButton).click();
     }
 }
