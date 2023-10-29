cd190cd8af724072b28294e76deeb35d18ae9dab
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-28 21:04:20 -0500

    finishes install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -37,29 +37,45 @@ run_command sudo systemctl enable docker
 echo "Starting MySQL database container..."
 run_command docker run --name mysql -e MYSQL_ROOT_PASSWORD="$mysql_password" -d -p 3306:3306 mysql:latest
 
-# Build and run the back-end API
-echo "Starting back-end API..."
-cd ../Back-End
-mvn clean install
-nohup java -jar target/javalin-api-1.0.0-SNAPSHOT.jar &
-
-# Run unit tests
+# Compiling Java back-end API and run unit tests
 echo "Running unit tests..."
+cd Back-End
+run_command mvn compile
+run_command mvn package
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore CategoryDALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore SessionDALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore EmployeeDALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore RequestDALTests 
+run_command java -cp target/classes ResetDatabaseTest
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore CategorySALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore SessionSALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore EmployeeSALTests 
+run_command java -cp target/classes:target/test-classes org.junit.runner.JUnitCore RequestSALTests 
+run_command java -cp target/classes ResetDatabaseTest
+
+# Running the back-end API
+echo "Starting back-end API..."
+nohup java -cp target/classes Run &
 
 # Install Node.js dependencies and start the React front-end
 echo "Starting front-end..."
-cd ../../front-end
+cd ../Front-End/front-end
 run_command npm install
-pm2 start npm --name "front-end" --start
+run_command pm2 start npm --name "front-end" -- dev
 
 # Run Selenium end-to-end tests
 echo "Running end-to-end tests..."
+run_command mvn test -Dcucumber.features=src/test/resources/Features
 
 # Reset database for production
 echo "Resetting database for production..."
+run_command java -cp target/classes ResetDatabaseProduction
 
 # Stop the back-end, front-end, and database
 echo "Stopping back-end, front-end, and database..."
+run_command pm2 stop "front-end"
+run_command pkill -f "java -cp target/classes Run"
+run_command docker stop mysql
 
 # Tell user if install is successful or unsuccessful (if statements will probably need to be included)
 if [ $failures -gt 0 ]; then
