23b2eb2d6f9abf08b35434d0e2fa66ce0786a504
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-28 21:05:18 -0500

    finishes stop script

--- Scripts/stop.sh
+++ Scripts/stop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Stop the React front-end
-pm2 delete front-end
+# Stop the front-end
+echo "Stopping front-end..."
+pm2 stop front-end
 
 # Stop the back-end API
+echo "Stopping back-end..."
+pkill -f "java -cp target/classes Run"
 
 # Stop the database
+echo "Stopping database..."
 docker stop mysql
+
+# Alert user of completion
+echo "Application stopped!"
