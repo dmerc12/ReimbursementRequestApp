b0922f603ae36662c1473968d6f6dccd291ec3b5
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-04 23:56:10 -0500

    completes manage requests page

---
+++ front-end/src/pages/ManageRequests.jsx
@@ -0,0 +1,29 @@
+import Cookies from 'js-cookie';
+import PropTypes from 'prop-types';
+
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RequestList } from 'components';
+
+export const ManageRequests = ({ toastRef }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const sessionId = Cookies.get('sessionId');
+        if (!sessionId) {
+            navigate('/login');
+            toastRef.current.addToast({ mode: 'warning', message: 'Please login or register to gain access!' });
+        }
+    }, []);
+
+    return (
+        <>
+            <h1>Manage Requests Below!</h1>
+            <RequestList toastRef={toastRef} />
+        </>
+    );
+};
+
+ManageRequests.propTypes = {
+    toastRef: PropTypes.object.isRequired
+};
