3874724e6f3154ae034f5f3fffd59f128f6da865
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-04 23:53:32 -0500

    completes use fetch

---
+++ front-end/src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+export const useFetch = () => {
+
+    const fetchData = async (url, requestMethod, requestBody) => {
+        try {
+            const response = await fetch(`http://localhost:8080${url}`, {
+                method: requestMethod,
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(requestBody)
+            });
+    
+            const result = await response.json();
+    
+            return {
+                data: result,
+                responseStatus: response.status
+            }
+        } catch (error) {
+            throw new Error("Failed to fetch")
+        }
+        
+    };
+
+    return {
+        fetchData
+    };
+};
