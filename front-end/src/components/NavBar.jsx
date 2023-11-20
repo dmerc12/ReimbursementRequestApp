ada0acefedd461b5e4dd8159dce602b8fad1fdcb
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-19 23:53:48 -0600

    adds custom use navigate hook that can be used elsewhere in project, still not rendering with an issue in the navbar right side

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from '../hooks';
 
 export const Navbar = ({ toastRef }) => {
     const navigate = useNavigate();
@@ -26,14 +26,14 @@ export const Navbar = ({ toastRef }) => {
                 </div>
                 <div className="nav-right">
                     {isLoggedIn ? (
-                        <>
+                        <div>
                             <button id="logoutButton" className="nav-item" onClick={handleLogout}>Logout</button>
-                        </>
+                        </div>
                     ) : (
-                        <>
+                        <div>
                             <Link className="nav-item" to='/login' id="loginTab">Login</Link>
                             <Link className="nav-item" to='/register' id="registerTab">Register</Link>
-                        </>
+                        </div>
                     )}
                 </div>
             </nav>
--- front-end/src/hooks/index.js
+++ front-end/src/hooks/index.js
@@ -1,3 +1,4 @@
 export * from './useFetch';
+export * from './useNavigate';
 export * from './useToast';
 export * from './useToastAutoClose';
